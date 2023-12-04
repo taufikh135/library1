9f6785a6527930f815e74fbfbf4711d7eeb481e2
Taufik Hidayat <opik.1320@gmail.com>
2023-12-04 08:22:47 +0700

    modify Customer

--- src/Customer.php
+++ src/Customer.php
@@ -3,8 +3,20 @@ namespace Taufik\Library;
 
 class Customer
 {
-    public function __construct()
+    public function __construct(private string $name)
     {
-        echo "Customer";
+        $this->name = $name;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function sayHello(): string
+    {
+        return "Hello {$this->getName()}";
     }
 }
